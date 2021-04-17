2c6bbfdad850f58857e3329c386a869f5ca6ed47
jmbeach <jaredbeachdesign@gmail.com>
2021-04-17 10:32:08 -0500

    flush-dns

--- ip.sh
+++ ip.sh
@@ -13,3 +13,7 @@ disable-ip6 () {
   sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
   sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 }
+
+flush-dns () {
+  sudo /etc/init.d/networking restart
+}
